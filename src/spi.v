7f997d28025f5fb9aec04eab441ed20c6e8806d8
bossboss852 <oreoluwaolwadare@gmail.com>
2026-05-16 12:32:58 -0400

    syntax-fixing

--- src/spi.v
+++ src/spi.v
@@ -1,9 +1,9 @@
 `default_nettype none
 
-module peripheral (
+module spi (
   input [2:0] ui_in,
   input clk, rst_n
-)
+);
 
 //data is ui_in[1], chip select is ui_in[2], sclk is ui_in[0]
 reg [2:0] clockstore;
@@ -18,42 +18,44 @@ reg [6:0] address;
 reg [7:0] data;
 reg write;
 always @(*) begin
-    if ((~ncsstore[1])&(state == IDLE)) begin
-      next = MODE;
+    if (~ncsstore[1]) begin
+      if (state == IDLE) begin
+        next = MODE;
+      end else if (state == MODE) begin
+        if (copistore[1]) begin
+          next = ADDRESS;
+        end else begin
+          next = IGNORE;
+        end
+      end else if (state == ADDRESS) begin
+        if (counter == 7) begin
+          next = BITINGEST;
+        end else begin
+          next = ADDRESS;
+        end
+      end else if (state == BITINGEST) begin
+        if (counter == 15) begin
+          next = IDLE;
+        end else begin
+          next = BITINGEST;
+        end
+      end else if (state == IGNORE) begin
+        if(counter == 15) begin
+          next = IDLE;
+        end else begin
+          next = IGNORE;
+        end
+      end
     end else begin
       next = IDLE;
-    end else if (state == MODE) begin
-      if (copistore[1]) begin
-        next = ADDRESS;
-      end else begin
-        next = IGNORE;
-      end
-    end else if (state == ADDRESS) begin
-      if (counter == 7) begin
-        next = BITINGEST;
-      end else begin
-        next = ADDRESS;
-      end
-    end else if (state == BITINGEST) begin
-      if (counter == 15) begin
-        next = IDLE;
-      end else begin
-        next = BITINGEST;
-      end
-    end else if (state == IGNORE) begin
-      if(counter == 15) begin
-        next = IDLE;
-      end else begin
-        next = IGNORE;
-      end
     end
 end
 
-always @(posedge clk and negedge rst_n) begin
+always @(posedge clk or negedge rst_n) begin
   if (~rst_n) begin
     counter <= 0;
     state <= IDLE;
-  end else if begin
+  end else begin
     state <= next;
     clockstore <= {clockstore[1:0], ui_in[0]};
     // the current sysclock is clockstore[1], last clock cycle is clockstore[2]
@@ -70,6 +72,8 @@ always @(posedge clk and negedge rst_n) begin
         address[counter] <= copistore[1];
       end else if (state == BITINGEST) begin
         data[(counter-7)] <= copistore[1];
+      end else if (state == IDLE) begin
+        counter <= 0;
       end
     end
   end
