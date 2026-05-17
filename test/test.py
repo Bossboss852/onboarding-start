cf17b0d851ab3f7119af70a2a3570f57a76b8048
bossboss852 <oreoluwaolwadare@gmail.com>
2026-05-17 18:05:58 -0400

    prep

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
-
+# The functions  test_pwm_freq, high_edge, low_edge, and test_pwm_duty were all written by Oreoluwa
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
